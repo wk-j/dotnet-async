b1e286da5ce0a460dbc85342b19212ba87838583
wk-j <somnuk.wk@gmail.com>
2018-05-15 01:21:19 +0700

    Initial commit

---
+++ src/DotNetAsync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
